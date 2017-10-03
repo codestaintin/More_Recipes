a0731b99efde3b3688850da0e88c8ccd17162091
Isioye Mohammed <mohzak1103@gmail.com>
2017-10-03 05:26:02 -0700

    Revert "Test"

--- server/models/category.js
+++
@@ -1,8 +0,0 @@
-const categoryModel = (sequelize, DataTypes) => {
-  const Category = sequelize.define('Category', {
-    name: { type: DataTypes.STRING }
-  });
-  return Category;
-};
-
-export default categoryModel;
