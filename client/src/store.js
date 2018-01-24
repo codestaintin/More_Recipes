018be567c458681d03dfa2dd2f9934e9b749ec1d
Isioye Mohammed <mohzak1103@gmail.com>
2018-01-24 16:29:36 +0100

    Revert "#151548481 Deploy base application"

--- client/src/store.js
+++ client/src/store.js
@@ -2,13 +2,8 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers/index';
 
-const reduxLogger = [];
-if (process.env.NODE_ENV === 'development') {
-  if (window.devToolsExtension) {
-    reduxLogger.push(window.devToolsExtension());
-  }
-}
-
-const store = createStore(reducers, {}, compose(applyMiddleware(thunk), ...reduxLogger));
+const store = createStore(reducers, {}, compose(applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
 
 export default store;
