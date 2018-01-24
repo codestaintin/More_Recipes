339f14fdc431c8f5b48d6efd43a5da19449cae36
Isioye Mohammed <mohammed.isioye@gmail.com>
2018-01-24 16:06:09 +0100

    chore(deploy-application): Deploy application to heroku
    - create webpack configuration for production
    - create tools directory for deployment configuration
    - add heroku build scripts to package.json
    - add compression and winston packages to package.json
    - add PrivateRoute.jsx to protected application routes
    - refactor store.js to hide redux logger in production environment
    
    [Finishes #151548481]

---
+++ production/script.js
@@ -0,0 +1,56 @@
+/* eslint-disable */
+// Create a clone of the menu, right next to original.
+/**
+ * Custom javascript file
+ */
+$('.recipe-image-thumb').click(function (e) {
+  let url = $(this).attr('src');
+  if (url) {
+    $('.recipe-big-img img').attr("src", url);
+  }
+  e.preventDefault();
+});
+
+$('.fav-btn').click(function (e) {
+  e.preventDefault();
+  let outline = 'btn-outline-warning';
+  let warning = 'btn-warning';
+  let $this = $(this);
+  if ($this.hasClass(outline)) {
+    $this
+      .removeClass(outline)
+      .addClass(`${warning} text-white`);
+  } else {
+    $this
+      .removeClass(`${warning} text-white`)
+      .addClass(outline);
+  }
+});
+
+$('.recipe-upload').change(function () {
+  let $this = $(this);
+  let file = this.files[0];
+  let reader = new FileReader();
+  let imageUrl = null;
+
+  reader.onload = function (event) {
+    imageUrl = event.target.result;
+    $this
+      .parent('.file-upload')
+      .css({'background-image': `url(${imageUrl})`, color: '#fff'});
+  };
+  reader.readAsDataURL(file);
+});
+
+$('.avatar-input').change(function () {
+  let $this = $(this);
+  let file = this.files[0];
+  let reader = new FileReader();
+  let imageUrl = null;
+
+  reader.onload = function (event) {
+    imageUrl = event.target.result;
+    $('.avatar').attr('src', imageUrl);
+  };
+  reader.readAsDataURL(file);
+});
